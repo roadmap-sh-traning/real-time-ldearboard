c7bc8bd49549ad0f3416f2d9dd94166f203b57f2
LukaTypeScripter <lukashinjikashvili85@gmail.com>
2026-05-22 20:44:46 +0400

    feat(redis): added redis

--- src/global.d.ts
+++ src/global.d.ts
@@ -9,6 +9,8 @@ import {
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import "@fastify/jwt";
 import { JwtPayload } from "./schemas";
+import "@fastify/redis";
+import { Redis } from "ioredis";
 
 declare module "fastify" {
   interface FastifyTypeProviderDefault extends TypeBoxTypeProvider {}
@@ -27,6 +29,13 @@ declare module "@fastify/jwt" {
   interface FastifyJWT {
     payload: { sub: number; email: string };
     user: JwtPayload;
+  }
+}
+
+declare module "fastify" {
+  interface FastifyRedis {
+    publisher: Redis;
+    subscriber: Redis;
   }
 }
 
