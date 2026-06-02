642b34faee0fcef5c130c346b07b928f65addc87
luka <shinjikashvili.luka22@gtu.ge>
2026-06-02 19:46:28 +0400

    chore(routes): remove redundant penalty redirect route for cleaner structure

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,10 +4,6 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (_request, _reply) {
     return { root: true };
   });
-
-  fastify.get("/penalty", async (_request, reply) => {
-    return reply.redirect("/penalty/");
-  });
 };
 
 export default root
